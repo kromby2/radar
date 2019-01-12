542905cdbf8c0b6775aeef513202368d80b76b36
M <mkloter@hotmail.com>
2019-01-12 20:57:32 +0000

    Inital Commit Ranging radar file decode only Not working as of commit

---
+++ chirp1.py
@@ -0,0 +1,17 @@
+import scipy as sp
+import matplotlib.pyplot as plt
+
+#SOL
+c=2.99e8
+
+#Starting and ending frequencys for chirp
+f0=5.9e9
+f1=6.0e9
+
+#Ramp time
+tramp=5.0e-3
+
+#Delay time for signal
+td=500e-6
+
+
